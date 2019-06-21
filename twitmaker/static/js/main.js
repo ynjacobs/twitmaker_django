e13ebd90e7c46bb2b597196f29531f894b1aa69c
ynjacobs <ytj64611@gamil.com>
2019-06-21 16:32:17 -0400

    adds tweets and puts in order

--- twitmaker/static/js/main.js
+++ twitmaker/static/js/main.js
@@ -1,13 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let form = document.querySelector('form');
+  const monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+    let form = document.querySelector('form');
   form.addEventListener('submit', function(event) {
     event.preventDefault();
+    
     let formData = new FormData(this);
+  
+  //  console.log(formData);
+  let ol = document.querySelector('.tweets');
+
     axios.post(
       this.action,
       formData,
     ).then(function(response) {
-      console.log(response);
+      console.log(response.data);
+      let data = response.data;
+      let li = document.createElement('li');
+      let time = document.createElement('time');
+      let p = document.createElement('p');
+      li.className = 'tweet';
+      
+      var myDate = new Date(data.created_at);
+      // var hola = myDate.forms
+      
+      zHours = myDate.getHours() > 12? myDate.getHours() - 12: myDate.getHours;
+      amOrpm = myDate.getHours() > 12? 'p.m.': 'a.m.';
+
+      let formattedDate = `${monthNames[myDate.getMonth()]} ${myDate.getDate()}, ${myDate.getFullYear()}, ${zHours}:${myDate.getMinutes()} ${amOrpm}`;    
+      time.innerText = formattedDate ;
+console.log(formattedDate);
+      // console.log('hola', hola);
+      p.innerText = data.message;
+      li.appendChild(time);
+      li.appendChild(p);
+      ol.appendChild(li);
+
+      let first = ol.getElementsByTagName('li')[0];
+      ol.insertBefore(li, first);
+      
+
+    
     }).catch(function(error) {
       console.log(error);
     });
--- twitmaker/views.py
+++ twitmaker/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.shortcuts import render
 from twitmaker.models import Tweet
 from twitmaker.forms import TweetForm
+import json
+import datetime, dateutil
+
 
 def index(request):
-    tweets = Tweet.objects.all()
+    tweets = Tweet.objects.all().order_by('-created_at')
     context = {'tweets': tweets, 'form': TweetForm()}
     return render(request, 'index.html', context)
 
@@ -14,7 +18,7 @@ def create_tweet(request):
     tweet = form.instance
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect('/')
+        return JsonResponse({'message': tweet.message, 'created_at': tweet.created_at})
     else:
         context = {'tweets': Tweet.objects.all(), 'form': form}
         return render(request, 'index.html', context)
